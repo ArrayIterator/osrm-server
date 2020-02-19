4068d6aa9c089d7b73fec47bf82f0225173ed175
arrayiterator <arrayiterator@gmail.com>
2020-02-20 00:58:34 +0700

    add change update

---
+++ public/sample/map/assets/js/config.js
@@ -0,0 +1,16 @@
+let config = {
+    selector: 'map',
+    type: 'google', // default provider
+    mode: 'standard', // default provider
+    // disable ESRI Map
+    disableMapEsri: true,
+    enableScale: false,
+    // disable Google Terrain
+    disableGoogleModeTerrain: true,
+    // disable all _mode satellite
+    disableModeSatellite: true,
+    // enable map control
+    enableMapControl: true,
+    preferCanvas: true,
+    fitSelectedRoutes: false
+};
